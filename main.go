e23de4e9ff692a1786de2ab62aa774b4823bb487
DOnEbrala <ebraebra13@gmail.com>
2022-02-17 21:26:38 +0400

    Changes

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
 func main() {
 
 	emoji.Println("Hello :world_map:!")
-	emoji.Println(":beer: Beer!!!")
-
-	pizzaMessage := emoji.Sprint("I like a :pizza: and :sushi:!!")
-	fmt.Println(pizzaMessage)
 }
